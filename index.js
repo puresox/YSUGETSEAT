441dbd681850350325ece6a8231bf392fa6e7261
puresox <puresox@163.com>
2018-07-20 12:43:09 +0800

    add log

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ async function login({ id, pwd }) {
     const {
       'set-cookie': [session],
     } = headers;
-    return session;
+    return { success: true, msg: session };
   }
-  return false;
+  return { success: false, msg: data.msg };
 }
 
 /**
@@ -40,16 +40,13 @@ async function login({ id, pwd }) {
  */
 async function getResvInfo(session) {
   const getResvIdUrl = 'http://seat.ysu.edu.cn/ClientWeb/pro/ajax/reserve.aspx?stat_flag=9&act=get_my_resv&_nocache=1531801794371';
-  const {
-    data: { data },
-  } = await axios.get(getResvIdUrl, {
+  const { data } = await axios.get(getResvIdUrl, {
     headers: { Cookie: session },
   });
-  if (data.length !== 0) {
-    const [{ id: resvId, devId, labId }] = data;
-    return { resvId, devId, labId };
+  if (data.ret !== 1) {
+    return { success: false, msg: data.msg };
   }
-  return false;
+  return { success: true, msg: data.data };
 }
 
 /**
@@ -59,9 +56,13 @@ async function getResvInfo(session) {
  */
 async function delResv(session, resvId) {
   const delResvUrl = `http://seat.ysu.edu.cn/ClientWeb/pro/ajax/reserve.aspx?act=del_resv&id=${resvId}&_nocache=1531823241100`;
-  await axios.get(delResvUrl, {
+  const { data } = await axios.get(delResvUrl, {
     headers: { Cookie: session },
   });
+  if (data.ret !== 1) {
+    return { success: false, msg: data.msg };
+  }
+  return { success: true, msg: '' };
 }
 
 /**
@@ -76,15 +77,71 @@ async function occupy(session, {
   resvId, devId, labId, start, end,
 }) {
   const occupyUrl = `http://seat.ysu.edu.cn/ClientWeb/pro/ajax/reserve.aspx?dev_id=${devId}&lab_id=${labId}&room_id=&kind_id=&type=dev&prop=&test_id=&resv_id=${resvId}&term=&min_user=&max_user=&mb_list=&test_name=&start=${start}&end=${end}&memo=&act=set_resv&_nocache=1531732522498`;
-  const {
-    data: { ret },
-  } = await axios.get(occupyUrl, {
+  const { data } = await axios.get(occupyUrl, {
     headers: { Cookie: session },
   });
-  return ret === 1;
+  if (data.ret !== 1) {
+    return { success: false, msg: data.msg };
+  }
+  return { success: true, msg: '' };
 }
 
 async function getSeat(user) {
+  // 登陆 获取session
+  let { success, msg } = await login(user);
+  if (!success) {
+    logger.error(`the user:${user.id} login error,system end. Error:${msg}`);
+    return;
+  }
+  const session = msg;
+  // 获取预约信息
+  ({ success, msg } = await getResvInfo(session));
+  if (!success) {
+    logger.error(`the user:${user.id} getResvInfo error,system end. Error:${msg}`);
+    return;
+  }
+  const reserves = msg;
+  const start = moment()
+    .add(20, 'm')
+    .format('YYYY-MM-DD HH:mm');
+  const end = moment().format('YYYY-MM-DD 22:30');
+  // 是否已经预约
+  if (reserves.length === 0) {
+    // 预约座位
+    const { devId, labId } = user;
+    ({ success, msg } = await occupy(session, {
+      resvId: '',
+      devId,
+      labId,
+      start,
+      end,
+    }));
+    if (success) {
+      logger.info(`the user:${user.id} reserves a new seat successfully`);
+    } else {
+      logger.error(`the user:${user.id} fail to reserve a new seat. Error:${msg}`);
+    }
+  } else {
+    const [{ id: resvId, devId, labId }] = reserves;
+    const info = { resvId, devId, labId };
+    // 占座 预约时间调到20分钟后
+    info.start = start;
+    info.end = end;
+    ({ success, msg } = await occupy(session, info));
+    if (!success) {
+      logger.error(`the user:${user.id} fail to change a reserve. Error:${msg}`);
+      ({ success, msg } = await delResv(session, info.resvId));
+      if (!success) {
+        logger.error(`the user:${user.id} fail to delete a reserve. Error:${msg}`);
+      }
+      logger.info(`the user:${user.id} delete a reserve successfully`);
+    } else {
+      logger.info(`the user:${user.id} change a reserve successfully`);
+    }
+  }
+}
+
+async function index() {
   const startTime = moment().format('YYYY-MM-DD 07:00');
   const endTime = moment().format('YYYY-MM-DD 21:30');
   // 是否为图书馆开馆时间
@@ -92,56 +149,15 @@ async function getSeat(user) {
     logger.warn('the library is close,system end');
     return;
   }
-  // 登陆 获取session
-  const session = await login(user);
-  if (!session) {
-    logger.error(`the user:${user.id} login error,system end`);
-    return;
-  }
-  // 获取预约信息
-  const info = await getResvInfo(session);
-  const start = moment()
-    .add(20, 'm')
-    .format('YYYY-MM-DD HH:mm');
-  const end = moment().format('YYYY-MM-DD 22:30');
-  // 是否已经预约
-  if (!info) {
-    // 预约座位
-    const { devId, labId } = user;
-    const occupyRes = await occupy(session, {
-      resvId: '',
-      devId,
-      labId,
-      start,
-      end,
-    });
-    if (occupyRes) {
-      logger.info(`the user:${user.id} reserves a new seat successfully`);
-    } else {
-      logger.error(`the user:${user.id} fail to reserves a new seat`);
-    }
-  } else {
-    // 占座 预约时间调到20分钟后
-    info.start = start;
-    info.end = end;
-    const occupyRes = await occupy(session, info);
-    if (!occupyRes) {
-      await delResv(session, info.resvId);
-      logger.info(`the user:${user.id} delete a reserve successfully`);
-    } else {
-      logger.info(`the user:${user.id} change a reserve successfully`);
-    }
-  }
-  // TODO: log && error
-}
-
-async function index() {
   logger.info('----------------------------------------------------------------------------');
-  const getSeatPromises = [];
-  config.forEach((user) => {
-    getSeatPromises.push(getSeat(user));
-  });
-  await Promise.all(getSeatPromises);
+  for (const user of config) {
+    await getSeat(user);
+  }
+  // const getSeatPromises = [];
+  // config.forEach((user) => {
+  //   getSeatPromises.push(getSeat(user));
+  // });
+  // await Promise.all(getSeatPromises);
   logger.info('----------------------------------------------------------------------------');
 }
 
